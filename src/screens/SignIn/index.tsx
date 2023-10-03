493ef64c6b9867fb248318cf958a6f805469ea19
jarilton <jarilton.pereira@grupofocus.com.br>
2023-10-02 22:03:51 -0300

    feat: estruturação do projeto

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export const SignIn = () => {
+  return (
+    <View style={styles.Container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </View>
+  );
+};
